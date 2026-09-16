fe4af363ea57803a08fb5744ea5015fae77654cf
Vitalii Martynyshyn <vitalikmartunushun@gmail.com>
2026-09-16 18:27:47 +0300

    initial commit

---
+++ src/test/java/ukma/jpay/JPayApplicationTests.java
@@ -0,0 +1,13 @@
+package ukma.jpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
